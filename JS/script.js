409033c4e681837f4d9054908a26378820849b57
Emeline <emeline.delarue31@gmail.com>
2024-04-10 14:56:11 +0200

    Fix du menu burger

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const burgerMenuButton = document.querySelector('.menu_burger');
-const menu = document.querySelector('.menu_contenu');
+const menu = document.querySelector('.menu_burger_contenu');
 
 burgerMenuButton.addEventListener('click', () => {
     menu.classList.toggle('open');
